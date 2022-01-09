fbaf36302892ccfa19f19b150ae12eefd5dbf4e8
Lee SJ <ei@genshin.ai>
2022-01-10 01:46:16 +0900

    #1010

---
+++ 10xx/1010.cpp
@@ -0,0 +1,24 @@
+// 정수형(int)으로 변수를 선언하고, 변수에 정수값을 저장한 후
+// 변수에 저장되어 있는 값을 그대로 출력해보자.
+
+// 예시
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     printf("%d", n);
+
+//     return 0;
+// }
+
+// 와 같은 형태로 가능하다. 
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    printf("%d", n);
+    return 0;
+}
