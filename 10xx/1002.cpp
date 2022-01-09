d48ee2c7f8437ce6c312f3203bc7d0815d6cbfbd
Lee SJ <ei@genshin.ai>
2022-01-10 00:43:08 +0900

    #1002

---
+++ 10xx/1002.cpp
@@ -0,0 +1,23 @@
+// 이번에는 공백()을 포함한 문장을 출력한다.
+// 다음 문장을 출력해보자.
+
+// Hello World
+// (대소문자에 주의한다.)
+
+// 참고
+// #include <stdio.h>
+
+// int main()
+// {
+//   printf("문장1 문장2");
+//   return 0;
+// }
+// 을 실행시키면 "문장1”, "문장2"가 공백()을 사이에 두고 출력된다.
+
+#include <stdio.h>
+
+int main()
+{
+  printf("Hello World");
+  return 0;
+}
