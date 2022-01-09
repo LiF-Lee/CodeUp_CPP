dd9411471db0645399d9ed415bf7fbf66618b61e
Lee SJ <ei@genshin.ai>
2022-01-10 01:06:00 +0900

    #1005

---
+++ 10xx/1005.cpp
@@ -0,0 +1,20 @@
+// 이번에는 큰따옴표(double quotation mark)가 포함된 출력문을 연습해보자.
+
+// 다음 문장을 출력하시오.
+
+// "Hello World"
+// (단, 큰따옴표도 함께 출력한다.)
+
+// 참고
+// printf( ) 함수로 큰 따옴표 문자 " 를 출력하기 위해서는 \" 로 출력해야 한다.
+
+// 예시
+// printf("\"");
+
+#include <stdio.h>
+
+int main()
+{
+    printf("\"Hello World\"");
+    return 0;
+}
