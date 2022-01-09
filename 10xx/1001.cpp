63ff360244155e9ad84e15a28a356623dd0e0cfb
Lee SJ <ei@genshin.ai>
2022-01-10 00:39:51 +0900

    #1001

---
+++ 10xx/1001.cpp
@@ -0,0 +1,25 @@
+// C/C++언어에서 가장 기본적인 명령이 출력문이다.
+// printf()를 이용해 다음 단어를 출력하시오.
+
+// Hello
+
+// 참고
+// C/C++ IDE 프로그램을 설치한 후,
+// 아래와 같은 소스 코드를 작성하고 "컴파일-실행"을 수행시키면,
+// 지정한 "문장"이 출력된다.
+
+// #include <stdio.h>
+
+// int main()
+// {
+//   printf("문장");
+//   return 0;
+// }
+
+#include <stdio.h>
+
+int main()
+{
+  printf("Hello");
+  return 0;
+}
